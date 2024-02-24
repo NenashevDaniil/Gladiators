d63af3a1768fdc1a32accee7c54092c52ff89c3c
NenashevDaniil <nenahev.daniil.02@gmail.com>
2024-02-24 17:08:58 +0300

    starter files

---
+++ Gladiators/Character.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiators
+{
+    class Character
+    {
+        int _health;
+        public int Health
+        {
+        get
+            {
+                return _health;
+            }
+          set
+            {
+                _health = value;
+
+            }
+
+
+        }
+        int _damage;
+        int _armor;
+        public Character()
+        {
+            _armor = 20;
+           
+            _damage = 50;
+
+        }
+        public void Attack()
+        {
+            _health = _health-(_damage - _armor);
+        }
+        public void HP()
+        {
+            Console.WriteLine($"Здоровье персонажа равно {Health}");
+                }
+    }
+}
---
+++ Gladiators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gladiators
+{
+    class Program
+    {
+        static void Main()
+        {
+            Character c = new Character();
+            c.Health = 100;
+            c.Attack();
+
+            c.HP();
+
+            
+
+        }
+    }
+}
